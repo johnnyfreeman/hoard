30ebb65c000a3c63b5972940a3ddd37f309f40a4
John Freeman <prsjohnny@gmail.com>
2023-05-04 23:53:30 -0400

    refactor

--- src/main.rs
+++ src/main.rs
@@ -2,133 +2,115 @@ use std::env;
 use std::fs;
 use std::io::Write;
 use std::process;
+use std::str::FromStr;
+use strum_macros::{Display, EnumString};
 
-trait Command {
-    fn handle(&self) -> i32;
+struct Tasks {
+    tasks: Vec<String>,
 }
 
-struct AddCommand {
-    args: Vec<String>,
-}
-
-impl AddCommand {
-    fn new(args: Vec<String>) -> Self {
-        AddCommand { args }
+impl Tasks {
+    fn get() -> Self {
+        Tasks {
+            tasks: fs::read_to_string("tasks.txt")
+                .expect("File not found.")
+                .lines()
+                .map(|line| line.to_string())
+                .collect::<Vec<String>>(),
+        }
     }
-}
 
-impl Command for AddCommand {
-    fn handle(&self) -> i32 {
-        let description = &self
-            .args
+    pub fn add(&mut self, descriptions: Vec<String>) -> &mut Self {
+        let description = descriptions
             .iter()
             .skip(2)
             .map(|arg| arg.to_string())
             .collect::<Vec<String>>()
             .join(" ");
 
-        let mut file = fs::OpenOptions::new()
-            .write(true)
-            .append(true)
-            .open("tasks.txt")
-            .expect("File not found.");
+        self.tasks.push(description);
 
-        writeln!(file, "{description}").expect("File not writable.");
-
-        return 0;
+        self
     }
-}
 
-struct ListCommand {}
+    pub fn list(self) -> Self {
+        println!("{}", self.tasks.join("\n"));
 
-impl ListCommand {
-    fn new() -> Self {
-        ListCommand {}
+        self
     }
-}
 
-impl Command for ListCommand {
-    fn handle(&self) -> i32 {
-        let tasks = fs::read_to_string("tasks.txt").expect("File not found.");
+    pub fn remove(&mut self, descriptions: Vec<String>) -> &mut Self {
+        for description in descriptions {
+            if let Some(index) = self.tasks.iter().position(|task| task == &description) {
+                self.tasks.remove(index);
+            } else {
+                eprintln!("Error: {:?} not found.", description);
+            }
+        }
 
-        println!("{tasks}");
-
-        0
+        self
     }
-}
 
-struct RemoveCommand {
-    args: Vec<String>,
-}
-
-impl RemoveCommand {
-    fn new(args: Vec<String>) -> Self {
-        RemoveCommand { args }
-    }
-}
-
-impl Command for RemoveCommand {
-    fn handle(&self) -> i32 {
-        let description = &self
-            .args
-            .iter()
-            .skip(2)
-            .map(|arg| arg.to_string())
-            .collect::<Vec<String>>()
-            .join(" ");
-
-        let tasks = fs::read_to_string("tasks.txt")
-            .expect("File not found.")
-            .lines()
-            .filter(|task| task != &description)
-            .collect::<Vec<&str>>()
-            .join("\n");
-
+    pub fn save(&self) -> i32 {
         let mut file = fs::OpenOptions::new()
             .write(true)
             .truncate(true)
             .open("tasks.txt")
             .expect("File not found.");
 
-        writeln!(file, "{tasks}").expect("File not writable.");
+        let contents = &self.tasks.join("\n");
+        writeln!(file, "{contents}").expect("File not writable.");
 
-        return 0;
+        0
     }
 }
 
-struct UnknownCommand {}
-
-impl UnknownCommand {
-    fn new() -> Self {
-        UnknownCommand {}
-    }
+#[derive(EnumString, Display)]
+enum Command {
+    #[strum(serialize = "add")]
+    Add,
+    #[strum(serialize = "remove")]
+    Remove,
+    #[strum(serialize = "list")]
+    List,
 }
 
-impl Command for UnknownCommand {
-    fn handle(&self) -> i32 {
-        println!("Unknown command.");
+fn parse_args(args: env::Args) -> (Command, Vec<String>) {
+    let binding = args.collect::<Vec<String>>();
+    let command = binding.get(1).unwrap_or_else(|| {
+        display_help();
 
-        1
-    }
+        std::process::exit(0);
+    });
+
+    let command = Command::from_str(command.as_str()).unwrap_or_else(|_| {
+        eprintln!("Error: Invalid command.");
+
+        process::exit(1);
+    });
+
+    return (
+        command,
+        binding
+            .iter()
+            .skip(2)
+            .map(|arg| arg.to_string())
+            .collect::<Vec<String>>(),
+    );
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let (command, args) = parse_args(env::args());
 
-    let command = args.get(1).unwrap_or_else(|| {
-        display_help();
+    let mut tasks = Tasks::get();
 
-        process::exit(0);
-    });
-
-    let exit_code: i32 = match command.as_str() {
-        "add" => AddCommand::new(args).handle(),
-        "list" => ListCommand::new().handle(),
-        "remove" => RemoveCommand::new(args).handle(),
-        _ => UnknownCommand::new().handle(),
+    let exit_code = match command {
+        Command::Add => tasks.add(args).save(),
+        Command::List => tasks.list().save(),
+        Command::Remove => tasks.remove(args).save(),
     };
 
-    process::exit(exit_code);
+    process::exit(exit_code)
 }
 
 fn display_help() {
@@ -137,5 +119,6 @@ fn display_help() {
     println!("Commands:");
     println!("   add <description> - adds task");
     println!("   list              - displays task list");
+    println!("   remove            - removes a task");
     println!();
 }
